6b8a922bd5bd62436d063d2d528b03e6c7761cb6
Vagner do Carmo <vluzrmos@gmail.com>
2015-06-03 17:16:53 -0300

    Link para ajudar a traduzir

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -1,13 +1,15 @@
 <li class="nav-docs"><a href="/docs">Documentação</a></li>
-<li class="nav-laracasts"><a href="https://codecasts.com.br">Codecasts</a></li>
+<li class="nav-codecasts"><a href="https://codecasts.com.br" target="_blank">Codecasts</a></li>
+<li class="nav-translations"><a href="https://github.com/artesaos/laravel-docs" target="_blank">Ajude a Traduzir</a></li>
+
 {{--<li class="nav-lumen"><a href="http://lumen.laravel.com">Lumen</a></li>--}}
 
 <li class="dropdown community-dropdown">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Comunidade <span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
-		<li><a href="https://github.com/artesaos/laravel-docs">GitHub</a></li>
+		<li><a href="https://github.com/artesaos" target="_blank">Artesãos</a></li>
 		<li class="divider"></li>
-		<li><a href="http://laravelbrasil.vluzrmos.com.br">Slack Laravel Brasil</a></li>
-        <li><a href="https://laravel.com.br">Blog Laravel Brasil</a></li>
+		<li><a href="http://laravelbrasil.vluzrmos.com.br" target="_blank">Slack Laravel Brasil</a></li>
+        <li><a href="https://laravel.com.br" target="_blank">Blog Laravel Brasil</a></li>
 	</ul>
 </li>
