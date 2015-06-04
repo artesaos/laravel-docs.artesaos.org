5aee1fc00d31fb83cddf7824983ebd5f12f0592f
Vagner do Carmo <vluzrmos@gmail.com>
2015-06-04 12:49:25 -0300

    detect https

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -2,6 +2,6 @@
 
 if(!function_exists('is_https')){
 	function is_https(){
-		return app('request')->isSecure();
+		return app('request')->isSecure() or app('request')->getScheme() =='https';
 	}
 }
