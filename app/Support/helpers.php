84aa5e185a6a3e60f5b6988616feff526fcfc0e8
Vagner do Carmo <vluzrmos@gmail.com>
2015-06-04 12:55:15 -0300

    fix parsing error

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -3,8 +3,12 @@
 if(!function_exists('is_https')){
 	function is_https(){
 		/** @var \Illuminate\Http\Request $request */
-		static $request = app('request');
+		static $request;
 
-		return $request->isSecure() or $request->getScheme() == 'https';
+		if(!$request){
+			$request = app('request');
+		}
+
+		return $request->isSecure() or ($request->getScheme() == 'https');
 	}
 }
