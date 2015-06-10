27ffa3898a05e7fb8c46e7bea3ce78cd34fa95d5
Vagner do Carmo <vluzrmos@gmail.com>
2015-06-10 00:57:53 -0300

    Add pseudo-branch 5.1

--- config/git-repos.php
+++ config/git-repos.php
@@ -1,13 +1,20 @@
 <?php return [
-	'enabled' => ['master', '5.0'],
+	'enabled' => ['master', '5.0', '5.1'],
 
 	'docs-versions' => [
 		'master' => 'Master',
+		'5.1'  => '5.1',
 		'5.0' => '5.0'
 	],
 
 	'repositories' => [
 		'master' => [
+			'url' => 'https://github.com/artesaos/laravel-docs.git',
+			'branch' => 'master',
+			'view-path' => 'pt_BR'
+		],
+
+		'5.1' => [
 			'url' => 'https://github.com/artesaos/laravel-docs.git',
 			'branch' => 'master',
 			'view-path' => 'pt_BR'
