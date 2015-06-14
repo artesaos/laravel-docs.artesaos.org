8a3d7c6c945aa31870c2e76b58a7a59a413da961
Vagner do Carmo <vluzrmos@gmail.com>
2015-06-14 14:16:43 -0300

    Update performance

--- app/Documentation.php
+++ app/Documentation.php
@@ -1,4 +1,6 @@
-<?php namespace App;
+<?php
+
+namespace App;
 
 use App\Services\GitRepoService;
 use Illuminate\Filesystem\Filesystem;
@@ -29,6 +31,24 @@ class Documentation {
 	protected $git;
 
 	/**
+	 * File of the index menu
+	 * @var string
+	 */
+	protected $indexFile = "documentation";
+
+	/**
+	 * Default cache time in minutes
+	 * @var int
+	 */
+	protected $cacheTime = 5;
+
+	/**
+	 * Extension of the files
+	 * @var string
+	 */
+	protected $filesExt = ".md";
+
+	/**
 	 * Create a new documentation instance.
 	 *
 	 * @param  Filesystem                  $files
@@ -50,39 +70,33 @@ class Documentation {
 	 */
 	public function getIndex($version)
 	{
-		return $this->cache->remember('docs.'.$version.'.index', 5, function() use ($version) {
-			$path = $this->git->getRepoViewsDir($version).'/documentation.md';
-
-			if ($this->files->exists($path)) {
-				return $this->replaceLinks($version, markdown($this->files->get($path)));
-			}
-
-			return null;
-		});
+		$contents = $this->get($version, $this->indexFile);
+		return $contents['content'];
 	}
 
 	/**
 	 * Get the given documentation page.
 	 *
-	 * @param  string  $version
-	 * @param  string  $page
+	 * @param  string $version
+	 * @param  string $page
+	 *
 	 * @return string
 	 */
 	public function get($version, $page)
 	{
-		$content = $this->cache->remember('docs.'.$version.'.'.$page, 5, function() use ($version, $page) {
-			$path = $this->git->getRepoViewsDir($version)."/{$page}.md";
+		return $this->cache->remember('docs.'.$version.'.'.$page, $this->cacheTime, function() use ($version, $page) {
+			$path = $this->fullPagePath($version, $page);
 
 			if ($this->files->exists($path)) {
-				return $this->replaceLinks($version, markdown($this->files->get($path)));
+				$content = $this->replaceLinks($version, markdown($this->files->get($path)));
+
+				$title = $content? $this->getDocTitleByContent($content): null;
+
+				return compact('content', 'title');
 			}
 
 			return null;
 		});
-
-		$title = $content? $this->getDocTitleByContent($content): null;
-
-		return compact('content', 'title');
 	}
 
 	/**
@@ -90,20 +104,34 @@ class Documentation {
 	 * @param  string $content mardown parsed string
 	 * @return string
 	 */
-	public function getDocTitleByContent($content){
+	public function getDocTitleByContent($content)
+	{
 		$title = (new Crawler(utf8_decode($content)))->filterXPath('//h1');
 		
-		return $title ? $title->text() : null;
+		return (count($title) ? $title->text() : null);
 	}
 
 	/**
 	 * Return default version of git repos
 	 * @return string by default will return "master"
 	 */
-	public function defaultVersion(){
+	public function defaultVersion()
+	{
 		return config('git-repos.default-version', 'master');
 	}
-	
+
+	/**
+	 * Get a full qualified path to a given page
+	 *
+	 * @param $version
+	 * @param $page
+	 *
+	 * @return string
+	 */
+	protected function fullPagePath($version, $page)
+	{
+		return $this->git->getRepoViewsDir($version)."/".$page.$this->filesExt;
+	}
 
 	/**
 	 * Replace the version place-holder in links.
@@ -126,14 +154,15 @@ class Documentation {
 	 */
 	public function sectionExists($version, $page)
 	{
-		return $this->files->exists($this->git->getRepoViewsDir($version)."/{$page}.md");
+		return $this->files->exists($this->fullPagePath($version, $page));
 	}
 
 	/**
 	 * Return an array [path => title] of the enabled docs
 	 * @return array
 	 */
-	public function getDocsEnabledVersions(){
+	public function getDocsEnabledVersions()
+	{
 		return $this->git->enabledRepos();
 	}
 
